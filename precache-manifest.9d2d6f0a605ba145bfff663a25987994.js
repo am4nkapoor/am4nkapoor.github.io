1da0a1b018c28725f397f78bf45f17f863131b2f
Aman Kapoor <aman.kapoor@collegedunia.com>
2020-05-09 17:35:06 +0530

    Updates

---
+++ precache-manifest.9d2d6f0a605ba145bfff663a25987994.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef10e5b086b2096b38267ba89135355c",
+    "url": "/am4nkapoor/index.html"
+  },
+  {
+    "revision": "8eb1a139840004716dc2",
+    "url": "/am4nkapoor/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "35cc684505e7dae4a81b",
+    "url": "/am4nkapoor/static/js/2.422f0243.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/am4nkapoor/static/js/2.422f0243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eb1a139840004716dc2",
+    "url": "/am4nkapoor/static/js/main.c864b902.chunk.js"
+  },
+  {
+    "revision": "ead578c93d5cbae0c175",
+    "url": "/am4nkapoor/static/js/runtime-main.7a99328a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/am4nkapoor/static/media/logo.5d5d9eef.svg"
+  }
+]);
